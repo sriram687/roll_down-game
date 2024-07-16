4a36aa0902cbe232e8282b9e0c18b67ef03ea635
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-16 14:05:48 +0530

    game over

--- roll.py
+++ roll.py
@@ -18,20 +18,21 @@ while True:
     else:
          print("The Entered Number is invalid")
 
-max_score = 50
+max_score = 20
 player_scores = [0 for _ in range(players)]
 
-while max(player_scores) < max_score:
 
+while max(player_scores) < max_score:
     for player_idx in range(players):
-        print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")        
         current_score = 0
 
         while True:
-            should_roll = input("Would you like to roll? (y)? ") 
+            should_roll = input("Would you like to roll (y/n)? ") 
             if should_roll.lower() != "y":
                 break
-            
+
             value = roll()
             if value == 1:
                 print("you rolled 1! turn done")
@@ -45,4 +46,9 @@ while max(player_scores) < max_score:
 
 
         player_scores[player_idx] += current_score
+        print(player_scores)
         print("Your total score is ", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of :", max_score)
