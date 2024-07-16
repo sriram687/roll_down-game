3eb557fe590a6963c787084bd6a4021be9652c75
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-16 12:09:25 +0530

    given no .of players to participate

--- roll.py
+++ roll.py
@@ -7,5 +7,15 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+while True:
+    players = input("Enter number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+             print("Must be between 2 - 4 players")
+    else:
+         print("The Entered Number is invalid")
+
+print(players)
