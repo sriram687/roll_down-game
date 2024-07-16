21d28594d5571f6d450cda66eac9063cc1016e54
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-16 13:11:48 +0530

    PLAYING started without the target

--- roll.py
+++ roll.py
@@ -23,17 +23,26 @@ player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
 
-    current_score = 0
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll? (y)? ") 
+            if should_roll.lower() != "y":
+                break
+            
+            value = roll()
+            if value == 1:
+                print("you rolled 1! turn done")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("you rolled :", value)
+
+            print("Your score is ", current_score)
 
 
-    should_roll = input("Would you like to roll? (y)? ") 
-    if should_roll.lower() == "y":
-        break
-    value = roll()
-    if value == 1:
-        print("you rolled 1! turn done")
-    else:
-        current_score += value
-        print("you rolled :", value)
-
-    print("Your score is ", current_score)
+        player_scores[player_idx] += current_score
+        print("Your total score is ", player_scores[player_idx])
